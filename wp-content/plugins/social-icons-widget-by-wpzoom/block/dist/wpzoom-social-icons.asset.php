6fd528397cf394b64361787215aa0bfcf0fc53f4
raphael gravagna <raphael.gravagna@laplateforme.io>
2022-02-02 11:08:57 +0100

    1

---
+++ wp-content/plugins/social-icons-widget-by-wpzoom/block/dist/wpzoom-social-icons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-token-list', 'wp-url'), 'version' => '5d89e90a2293f464d077ac411945dac6');
